e55d1d486c7947a9c554512e82a76e724c757814
4MHARK <mharkdavid713@gmail.com>
2024-03-01 15:28:00 -0800

    created slider js

--- assets/js/toogleswitch.js
+++ assets/js/toogleswitch.js
@@ -1,46 +1,45 @@
-const body = document.querySelector('body');
-const btn = document.querySelector('.btn');
-const icon = document.querySelector('.btn__icon');
+var divs = document.querySelectorAll('.show-section section');
+var now = 0; // currently shown div
 
-
-
-
-
-function store(value){
-    localStorage.setItem('darkmode', value);
-}
-
-
-
-function load(){
-    const darkmode = localStorage.getItem('darkmode');
-    const body = document.querySelector('body');
-
-    if(!darkmode){
-        store(false);
+// Hide all divs except the first one
+divs.forEach(function(div, index) {
+    if (index !== 0) {
+        div.style.display = 'none';
     }
-    else if (darkmode == 'true'){
-        body.classList.add('darkmode');
-    }
-}
-load();
-btn.addEventListener('click', () => {
-    body.classList.toggle('darkmode');
-    icon.classList.add('animated')
+});
 
-store(body.classList.contains('darkmode'))
-
-if(body.classList.contains('darkmode')){
-    icon.classList.remove('//the icon name like the one for material  the sun');
-    icon.classList.add('//the icon name like the one for material the moon');
-}
-else{
-    icon.classList.remove('//moon')
-    icon.classList.add('//sun')
+// Function to handle next button click
+function next() {
+    divs[now].style.display = 'none';
+    now = (now + 1 < divs.length) ? now + 1 : 0;
+    divs[now].style.display = 'block'; // show next
+    showActiveStep();
 }
 
-setTimeout( () => {
-    
- icon.classList.remove('animated');
-}, 500)
-})
+// Adding click event listener to elements with class "prev"
+document.querySelectorAll('.prev').forEach(function(button) {
+    button.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the default behavior of anchor tag
+        divs[now].style.display = 'none';
+        now = (now > 0) ? now - 1 : divs.length - 1;
+        divs[now].style.display = 'block'; // show previous
+        showActiveStep();
+    });
+});
+
+// Adding click event listener to button with class "next"
+document.querySelector('.next').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default behavior of anchor tag
+    next();
+});
+
+// Function to show active step
+function showActiveStep() {
+    var stepCounterDivs = document.querySelectorAll(".step-counter-inner div");
+    stepCounterDivs.forEach(function(stepCounterDiv, index) {
+        stepCounterDiv.classList.remove("active");
+        if (index === now) {
+            stepCounterDiv.classList.add("active");
+        }
+    });
+}
